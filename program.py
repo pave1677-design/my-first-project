27418c18982f4d1be55e8175033d78289beddd77
iruyisjidsidsjivdsij <pave1677@yandex.ru>
2025-12-02 12:35:46 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,1 +1,18 @@
-print("Hello, GitHub!") 
+from prettytable import PrettyTable
+import matplotlib.pyplot as diagram
+receipt = PrettyTable()
+receipt.field_names = ['№', 'product', 'price', 'quantity', 'finalprice']
+
+products = []
+pay = []
+
+for i in range(3):
+    product = input('enter product name: ')
+    price = int(input('enter product price: '))
+    quantity = int(input('enter product quantity: '))
+    receipt.add_row([i+1, product, price, quantity, price * quantity])
+    products.append(product)
+    pay.append(price*quantity)
+print(receipt)
+diagram.plot(products.pay)
+diagram.show()
